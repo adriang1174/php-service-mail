e1af784bcba4ed5680c5ea0b9c1d69f99959472e
Adrian <adriang_1174@hotmail.com>
2016-01-05 15:07:40 -0700

    parametizing

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$from = 'info@labolatorioraffo.com';
+$from_name = 'Laboratorio Raffo';
+$to = 'adriang_1174@hotmail.com';
+$to_name = 'Adrian';
+$subject = 'PHPMailer sendmail test';
+$message = 'This is a test';
+
+?>
+
+
